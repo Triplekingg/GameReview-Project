8308d141caf6285e38090a25c238a8ffcac3d4e5
harinkhanijou <simarkhanijou@gmail.com>
2021-07-16 14:58:39 +0700

    blah testing 2

--- src/main/java/com/example/securingweb/blahhh.java
+++ src/main/java/com/example/securingweb/blahhh.java
@@ -3,15 +3,7 @@ package com.example.securingweb;
 public class blahhh {
 
 
-    public void pleaseWork(){
-        System.out.println("if you see this means it works");
-        System.out.println("fuck you");
-        System.out.println("fuck 2");
-        System.out.println("mother fucker");
-        System.out.println("mother fucker 2");
 
-    }
-    //this is to test
 
 
     public static void main(String[] args) {
@@ -22,6 +14,17 @@ public class blahhh {
 
     public int sum(){
         return 1 + 2;
+    }
+
+}
+
+
+    public int testing1(){
+        return 1;
+    }
+
+    public String hi(){
+        return "yo";
     }
 
 }
