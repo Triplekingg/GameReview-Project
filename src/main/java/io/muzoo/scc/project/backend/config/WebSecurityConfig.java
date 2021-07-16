288f81070392afe679f10edb80b36ca3b846e63e
Manit Gandhi <yourgitemail@example.com>
2021-07-16 17:20:16 +0700

    Continue set up, refactor package, create config directory for configuration files

--- src/main/java/io/muzoo/scc/project/backend/config/WebSecurityConfig.java
+++ src/main/java/io/muzoo/scc/project/backend/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.securingweb;
+package io.muzoo.scc.project.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
