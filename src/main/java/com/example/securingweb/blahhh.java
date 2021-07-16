68e2f9d4243e31ca310e541f9452a03fe862397c
harinkhanijou <simarkhanijou@gmail.com>
2021-07-16 14:44:43 +0700

    blah testing

--- src/main/java/com/example/securingweb/blahhh.java
+++ src/main/java/com/example/securingweb/blahhh.java
@@ -5,6 +5,18 @@ public class blahhh {
 
     public void pleaseWork(){
         System.out.println("if you see this means it works");
+        System.out.println("fuck you");
+        System.out.println("fuck 2");
+        System.out.println("mother fucker");
+        System.out.println("mother fucker 2");
+
+    }
+    //this is to test
+
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+
     }
 
 
