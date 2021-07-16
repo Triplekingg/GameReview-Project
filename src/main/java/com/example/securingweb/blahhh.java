99b3aa922254fd88c86b83df3ec157af48c1ea58
Rathanon <rathanon.sin@student.mahidol.edu>
2021-07-16 14:39:17 +0700

    hello

--- src/main/java/com/example/securingweb/blahhh.java
+++ src/main/java/com/example/securingweb/blahhh.java
@@ -1,6 +1,13 @@
 package com.example.securingweb;
 
 public class blahhh {
+
+
+    public void pleaseWork(){
+        System.out.println("if you see this means it works");
+        System.out.println("fuck you");
+        System.out.println("fuck 2");
+    }
     //this is to test
 
 
