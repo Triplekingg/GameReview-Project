091a5c97295261b6e474ccd7c258a5341cb587b4
Rathanon <rathanon.sin@student.mahidol.edu>
2021-07-17 16:51:05 +0700

    removed authentication controller

--- src/main/java/io/muzoo/ssc/project/backend/config/WebSecurityConfig.java
+++ src/main/java/io/muzoo/ssc/project/backend/config/WebSecurityConfig.java
@@ -1,63 +1,49 @@
+
 package io.muzoo.ssc.project.backend.config;
 
 import io.muzoo.ssc.project.backend.auth.OurUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
 	@Autowired
 	private OurUserDetailsService ourUserDetailsService;
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/","/api/login","/api/logout").permitAll();
-		http.authorizeRequests().antMatchers(HttpMethod.OPTIONS,"/**").permitAll();
-		http.exceptionHandling().authenticationEntryPoint(new JsonHttp403ForbiddenEntryPoint());
-		http.authorizeRequests().antMatchers("/**");
+		http
+				.authorizeRequests()
+				.antMatchers("/", "/home").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+				.logout()
+				.permitAll();
 	}
-
 	@Bean
 	public PasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
 	}
-
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
 	}
-
 	@Override
 	public UserDetailsService userDetailsService() {
 		return ourUserDetailsService;
 	}
-
-	class JsonHttp403ForbiddenEntryPoint implements AuthenticationEntryPoint {
-
-		@Override
-		public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
-			httpServletResponse.getWriter().println("You are not allowed to access this");
-
-		}
-	}
-
 }
