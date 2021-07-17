b1b237fa63c071341537f3792dd415efa5fd1f3d
Rathanon <rathanon.sin@student.mahidol.edu>
2021-07-17 18:17:13 +0700

    added authentication controller (working)

--- src/main/java/io/muzoo/ssc/project/backend/config/WebSecurityConfig.java
+++ src/main/java/io/muzoo/ssc/project/backend/config/WebSecurityConfig.java
@@ -5,14 +5,23 @@ import io.muzoo.ssc.project.backend.auth.OurUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.script.ScriptContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
 @Configuration
@@ -20,24 +29,32 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private OurUserDetailsService ourUserDetailsService;
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-				.authorizeRequests()
-				.antMatchers("/", "/home").permitAll()
-				.anyRequest().authenticated()
-				.and()
-				.formLogin()
-				.loginPage("/login")
-				.permitAll()
-				.and()
-				.logout()
-				.permitAll();
-	}
+
 	@Bean
 	public PasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//configuring security for REST backend APIs
+		// disable csrf
+		http.csrf().disable();
+		//Permit root and /api/login and /api/logout
+		http.authorizeRequests()
+				.antMatchers("/", "/api/login", "/api/logout").permitAll();
+		//permit all OPTIONS requests
+		http.authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+
+		//Handle error output
+		http.exceptionHandling()
+				.authenticationEntryPoint(new JsonHttp403ForbiddenEntryPoint());
+
+		// set every other path to require authentication
+		http.authorizeRequests().antMatchers("/**/").authenticated();
+
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
@@ -46,4 +63,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public UserDetailsService userDetailsService() {
 		return ourUserDetailsService;
 	}
+
+	class JsonHttp403ForbiddenEntryPoint implements AuthenticationEntryPoint{
+
+		@Override
+		public void commence(HttpServletRequest request,
+							 HttpServletResponse response,
+							 AuthenticationException authException) throws IOException, ServletException {
+			// output JSON message
+			response.getWriter().println("You are not allowed to access this.");
+		}
+	}
+
 }
